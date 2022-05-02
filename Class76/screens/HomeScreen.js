1fa5d5a1a47bf5140d2c162f2bee3d3c3b564c48
AmoghMerudi <73833979+AmoghMerudi@users.noreply.github.com>
2022-05-02 18:52:00 +0530

    Add files via upload

---
+++ Class76/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+export default class HomeScreen extends React.Component
+{
+    render(){
+        return(
+            <View>
+                <Text>Welcome to HomeScreen</Text>
+            </View>
+        );
+    }
+}
